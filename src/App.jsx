dbfa7f57c118c9c6d3284435cdb376d97ae12bba
http43 <dilshadgms@gmail.com>
2025-09-04 09:54:15 +0500

    office pc

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Components/Home'
+import { BrowserRouter } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-       <NavBar />
-    </BrowserRouter>
+   <BrowserRouter>
+     <NavBar />
+   </BrowserRouter>
+     
+   
    
       <Home />
     </>
