62dc5a7192429be030a629fc86750abda9cefd03
Dilshad Ahmad <dilshadgms@gmail.com>
2025-09-06 12:03:27 +0500

    Main home

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,80 +1,91 @@
 import React from 'react'
 import './NavBar.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-{/* <script type="text/javascript" src="js/bootstrap.min.js"></script> */}
+{/* <script type="text/javascript" src="js/bootstrap.min.js"></script> */ }
 import { NavLink, Outlet } from 'react-router-dom';
 const NavBar = () => {
 
-    return (
-        <>
-       <nav className="navbar navbar-expand-lg bg-gray">
-  <div className="container-fluid">
-    <NavLink className="navbar-brand" to="/">InfoPk </NavLink>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-        </li>
-        {/* <li className="nav-item"> <NavLink className="nav-link" to="/aiou">AIOU </NavLink></li> */}
-         <li className="nav-item dropdown">
-          <NavLink className="nav-link dropdown-toggle" to="aiou" role="button" 
-          // data-bs-toggle="dropdown" aria-expanded="false"
-          >
-            AIOU 
-          </NavLink>
-          <ul className="dropdown-menu">
-            <li><NavLink className="dropdown-item" to="aiou-admission">AIOU Admission </NavLink></li>
-            <li><NavLink className="dropdown-item" to="aiou-assignment">AIOU Assignment </NavLink></li>
-            <li><NavLink className="dropdown-item" to="aiou-result">AIOU Result </NavLink></li>
-          </ul>
-        </li>
-      
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-secondary bg-custom">
+        <div className="container-fluid w-75">
+          <NavLink className="navbar-brand text-white" to="/">InfoPk </NavLink>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav ">
+              <li className="nav-item">
+                <NavLink className="nav-link active text-white" aria-current="page" to="/">Home</NavLink>
+              </li>
+
+              <li className="nav-item dropdown">
+                <NavLink className="nav-link dropdown-toggle text-white" to="/aiou" role="button">
+                  AIOU
+                </NavLink>
+                <ul className="dropdown-menu bg-dark">
+                  <li><NavLink className="dropdown-item text-danger" to="aiou-admission">AIOU Admission </NavLink></li>
+                  <li><NavLink className="dropdown-item text-info" to="aiou-assignment">AIOU Assignment </NavLink></li>
+                  <li><NavLink className="dropdown-item text-info" to="aiou-result">AIOU Result </NavLink></li>
+                </ul>
+              </li>
+
+              <li className="nav-item dropdown">
+                <NavLink className="nav-link dropdown-toggle text-white" to="/bise" role="button">
+                  BISE
+                </NavLink>
+                <ul className="dropdown-menu bg-dark">
+                  <li><NavLink className="dropdown-item text-primary" to="/bise-admission">BISE Admission</NavLink></li>
+                  <li><NavLink className="dropdown-item text-primary" to="/bise-registration">BISE Registration</NavLink>
+                  </li>
 
 
-<li className="nav-item dropdown">  
-  <NavLink     className="nav-link dropdown-toggle"     to="/bise"    role="button"
-  >    BISE  </NavLink>
+                  <li className="nav-item dropdown submenus">
+                    <NavLink className="nav-link dropdown-toggle text-white " to="/bise" role="button">
+                      BISE Next
+                    </NavLink>
+                    <ul className="dropdown-menu bg-dark subli">
+                      <li><NavLink className="dropdown-item text-white" to="/bise-admission">BISE Admission</NavLink></li>
+                      <li><NavLink className="dropdown-item text-white" to="/bise-registration">BISE Registration</NavLink></li>
+                      <li><NavLink className="dropdown-item text-white" to="/bise-result">BISE Result</NavLink></li>
+                    </ul>
+                  </li>
 
-  <ul className="dropdown-menu">
-    <li><NavLink className="dropdown-item" to="/bise-admission">BISE Admission</NavLink></li>
-    <li><NavLink className="dropdown-item" to="/bise-registration">BISE Registration</NavLink></li>
-    <li><NavLink className="dropdown-item" to="/bise-result">BISE Result</NavLink></li>
-  </ul>
-</li>
-        
-        {/* <li className="nav-item">          <NavLink className="nav-link" to="/bise">BISE </NavLink>        </li> */}
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/iub">IUB </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/jobs">Latest Jobs </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/online">Online Earning </NavLink>
-        </li>
-        <li className="nav-item dropdown">
-          <NavLink className="nav-link dropdown-toggle" to="/other" role="button" 
-          // data-bs-toggle="dropdown" aria-expanded="false"
-          >
-            Other 
-          </NavLink>
-          <ul className="dropdown-menu">
-            <li><NavLink className="dropdown-item" to="#">Action</NavLink></li>
-            <li><NavLink className="dropdown-item" to="#">Another action</NavLink></li>
-            <li><NavLink className="dropdown-item" to="#">Something else here</NavLink></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>   
-<Outlet/>
 
-        </>
-    )
+                  <li><NavLink className="dropdown-item text-white" to="/bise-result">BISE Result</NavLink></li>
+                </ul>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link text-white text-danger.active" to="/iub">IUB</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link text-white" to="/jobs">Latest Jobs</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link text-white" to="/online">Online Earning</NavLink>
+              </li>
+
+
+              <li className="nav-item dropdown">
+                <NavLink className="nav-link dropdown-toggle text-white" to="/bise" role="button">
+                  BISE
+                </NavLink>
+                <ul className="dropdown-menu bg-dark">
+                  <li><NavLink className="dropdown-item text-white" to="/bise-admission">BISE Admission</NavLink></li>
+                  <li><NavLink className="dropdown-item text-white" to="/bise-registration">BISE Registration</NavLink></li>
+                  <li><NavLink className="dropdown-item text-white" to="/bise-result">BISE Result</NavLink></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Outlet />
+
+    </>
+  )
 }
 
 export default NavBar
