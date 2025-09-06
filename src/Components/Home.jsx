7633c12f4b94080dbaae6ea02739d35dc5dd7ab7
Dilshad Ahmad <dilshadgms@gmail.com>
2025-09-06 13:26:45 +0500

    home pc

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import './Home.css'
+import HomeBoxes from './HomeBoxes'
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='hero-container'>
+            <div className="hero">
+
+                <div className="hero-text">
+                    <h1 className='span'>The Easiest Way to <br /> Get Your New Future</h1> <br />
+                    <input type="text" name="search" id="" placeholder='Search Anything' />
+                    <button className='btn-primary' id='search'>Search </button>
+                </div>
+            </div>
+            <HomeBoxes />
+
+
+
+        </div>
     )
 }
 
---
+++ src/Components/HomeBoxes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeBoxes = () => {
+    return (
+        <div>HomeBoxes</div>
+    )
+}
+
+export default HomeBoxes
